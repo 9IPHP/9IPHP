e1b632cf2caa985bbe40f0042b4492d7b8009149
Specs <464089419@qq.com>
2014-11-03 21:33:55 +0800

    优化摘要显示信息

--- inc/post-format/content.php
+++ inc/post-format/content.php
@@ -34,12 +34,7 @@
 	</header>
 	<div class="entry-summary">
 		<?php 
-			$excerpt = get_the_excerpt(); 
-			if(!$excerpt){
-				echo mb_strimwidth(strip_tags(apply_filters('the_content', $post->post_content)), 0, 300,"……"); 
-			}else{
-				echo $excerpt;
-			}
+			the_excerpt(); 
 		?>
 	</div>
 	<footer class="entry-footer clearfix visible-lg visible-md visible-sm">
