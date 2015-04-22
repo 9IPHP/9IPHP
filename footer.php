e1a7366b7b1562b762645a113614ed46dd3b761a
Specs <464089419@qq.com>
2015-04-22 14:08:57 +0800

    delete a file

--- footer.php
+++ footer.php
@@ -54,39 +54,6 @@
     </ul>
 </footer>
 <?php wp_footer(); ?>
-<script type="text/javascript">// <![CDATA[
-$.fn.smartFloat = function() {
-    var position = function(element) {
-        var top = element.position().top, pos = element.css("position");
-        $(window).scroll(function() {
-            var scrolls = $(this).scrollTop();
-            if (scrolls > top) {
-                if (window.XMLHttpRequest) {
-                    element.css({
-                        position: "fixed",
-                        top: "65px"
-                    });
-                } else {
-                    element.css({
-                        top: scrolls
-                    });
-                }
-            }else {
-                element.css({
-                    position: pos,
-                    top: top
-                });
-            }
-        });
-    };
-    return $(this).each(function() {
-        position($(this));
-    });
-};
-
-//绑定
-$("#float").smartFloat();
-// ]]></script>
 
 </body>
 </html>
