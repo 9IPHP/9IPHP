0aa2d779becf0e104b35527c93c384f1c4086beb
Specs <464089419@qq.com>
2015-05-06 14:24:59 +0800

    更新标签页汉字转拼音方法

--- functions.php
+++ functions.php
@@ -610,62 +610,6 @@ function most_comm_posts($days=30, $nums=5) { //$days参数限制时间值，单
 	echo $output;
 }
 /**
- * 9IPHP <Get post title first letter> in the theme.
- * 拼音首字母
- *
- * @version 1.0
- * @package Specs
- * @copyright 2014 all rights reserved
- *
- */
-function specs_getfirstchar($s0){
-	$fchar = ord($s0{0});
-	if($fchar >= ord("A") and $fchar <= ord("z") )return strtoupper($s0{0});
-	$s1 = iconv("UTF-8","gb2312", $s0);
-	$s2 = iconv("gb2312","UTF-8", $s1);
-	if($s2 == $s0){$s = $s1;}else{$s = $s0;}
-	$asc = ord($s{0}) * 256 + ord($s{1}) - 65536;
-	if($asc >= -20319 and $asc <= -20284) return "A";
-	if($asc >= -20283 and $asc <= -19776) return "B";
-	if($asc >= -19775 and $asc <= -19219) return "C";
-	if($asc >= -19218 and $asc <= -18711) return "D";
-	if($asc >= -18710 and $asc <= -18527) return "E";
-	if($asc >= -18526 and $asc <= -18240) return "F";
-	if($asc >= -18239 and $asc <= -17923) return "G";
-	if($asc >= -17922 and $asc <= -17418) return "H";
-	if($asc >= -17417 and $asc <= -16475) return "J";
-	if($asc >= -16474 and $asc <= -16213) return "K";
-	if($asc >= -16212 and $asc <= -15641) return "L";
-	if($asc >= -15640 and $asc <= -15166) return "M";
-	if($asc >= -15165 and $asc <= -14923) return "N";
-	if($asc >= -14922 and $asc <= -14915) return "O";
-	if($asc >= -14914 and $asc <= -14631) return "P";
-	if($asc >= -14630 and $asc <= -14150) return "Q";
-	if($asc >= -14149 and $asc <= -14091) return "R";
-	if($asc >= -14090 and $asc <= -13319) return "S";
-	if($asc >= -13318 and $asc <= -12839) return "T";
-	if($asc >= -12838 and $asc <= -12557) return "W";
-	if($asc >= -12556 and $asc <= -11848) return "X";
-	if($asc >= -11847 and $asc <= -11056) return "Y";
-	if($asc >= -11055 and $asc <= -10247) return "Z";
-	return null;
-}
-function specs_pinyin($zh){
-	$ret = "";
-    $s1 = iconv("UTF-8","gb2312", $zh);
-    $s2 = iconv("gb2312","UTF-8", $s1);
-    if($s2 == $zh){$zh = $s1;}
-    $s1 = substr($zh,$i,1);
-    $p = ord($s1);
-    if($p > 160){
-        $s2 = substr($zh,$i++,2);
-        $ret .= specs_getfirstchar($s2);
-    }else{
-        $ret .= $s1;
-    }
-	return strtoupper($ret);
-}
-/**
  * 9IPHP <Get all tags> in the theme.
  * 标签页面
  *
@@ -676,6 +620,7 @@ function specs_pinyin($zh){
  */
 function specs_show_tags() {
 	if(!$output = get_option('specs_tags_list')){
+        include_once('inc/zh_to_py.php');
 		$categories = get_terms( 'post_tag', array(
 			'orderby'    => 'count',
 			'hide_empty' => 1
@@ -683,12 +628,12 @@ function specs_show_tags() {
 		$r = array();
 		foreach($categories as $v){
 			for($i = 65; $i <= 90; $i++){
-				if(specs_pinyin($v->name) == chr($i)){
+				if(strtoupper(Chinese_to_PY::getPY($v->name,'one')) == chr($i)){
 					$r[chr($i)][] = $v;
 				}
 			}
 			for($i=48;$i<=57;$i++){
-				if(specs_pinyin($v->name) == chr($i)){
+				if(strtoupper(Chinese_to_PY::getPY($v->name,'one')) == chr($i)){
 					$r[chr($i)][] = $v;
 				}
 			}
