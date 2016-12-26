582d29d7300f8d7064441dc519655f5dd8e83bec
Specs <464089419@qq.com>
2016-12-26 12:05:35 +0800

    remove useless class

--- functions.php
+++ functions.php
@@ -32,8 +32,7 @@ add_action( 'after_setup_theme', 'specs_register_nav_menu' );
 class Bootstrap_Walker extends Walker_Nav_Menu
 {
     function start_lvl( &$output, $depth = 0, $args = array() )
-    {
-        $tabs = str_repeat("\t", $depth);
+    {    $tabs = str_repeat("\t", $depth);
         // If we are about to start the first submenu, we need to give it a dropdown-menu class
         if ($depth == 0 || $depth == 1) { //really, level-1 or level-2, because $depth is misleading here (see note above)
             $output .= "\n{$tabs}<ul class=\"dropdown-menu\">\n";
@@ -121,47 +120,7 @@ class Bootstrap_Walker extends Walker_Nav_Menu
         return parent::display_element($element, $children_elements, $max_depth, $depth, $args, $output);
     }
 }
-//改变菜单输出样式以适应Bootstrap菜单
-class themeslug_walker_nav_menu extends Walker_Nav_Menu {
-    // add classes to ul sub-menus
-    function start_lvl( &$output, $depth ) {
-        // depth dependent classes
-        $indent = ( $depth > 0  ? str_repeat( "\t", $depth ) : '' ); // code indent
-        $display_depth = ( $depth + 1); // because it counts the first submenu as 0
 
-        // build html
-        $output .= "\n" . $indent . '<ul class="dropdown-menu">' . "\n";
-    }
-    // add main/sub classes to li's and links
-    function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-        global $wp_query;
-        $indent = ( $depth > 0 ? str_repeat( "\t", $depth ) : '' ); // code indent
-
-        // passed classes
-        $classes = empty( $item->classes ) ? array() : (array) $item->classes;
-        $class_names = esc_attr( implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) ) );
-        // build html
-        $output .= $indent . '<li class="' . $class_names . '">';
-
-        // link attributes
-        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
-        $attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
-        $attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
-        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
-        $attributes .= ! empty( $item->data_toggle ) ? ' data-toggle="'   . esc_attr( $item->data_toggle ) .'"' : '';
-        $attributes .= ! empty( $item->a_class ) ? ' class="'   . esc_attr( $item->a_class ) .'"' : '';
-        $item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
-            $args->before,
-            $attributes,
-            $args->link_before,
-            apply_filters( 'the_title', $item->title, $item->ID ),
-            $args->link_after,
-            $args->after
-        );
-        // build html
-        $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-    }
-}
 //修改拥有二级菜单的顶级菜单项
 add_filter( 'wp_nav_menu_objects', 'add_menu_parent_class' );
 function add_menu_parent_class( $items ) {
